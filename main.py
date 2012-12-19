ce84f1fa7467bba435c3ad9babaa0676b4c4c798
Steve Vaught <Steven.D.Vaught@gmail.com>
2012-12-19 15:34:34 -0500

    Added support for private credentials.  Fixes #3

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 
-#import 
+import pickle
+
+def sendText(message):
+    '''Send a text containing a message'''
+    
+    #TODO: create network code here
+    
+    #placeholder
+    print( "Simulated sending of: {}".format(message))
+    
+    return
+
+def getCredentials():
+    
+    with open('./PRIVATE/credentials','rb') as newCredentials:
+        credentials = pickle.loads(newCredentials.read())
+    
+    return credentials
 
 def main():
     
-    print("Nothing here yet")
-    
+    getCredentials()
     
 if __name__ == '__main__':
     main()
