a2148dae6ad7557b875e96338fb4857ee8398286
Steve Vaught <Steven.D.Vaught@gmail.com>
2012-12-19 16:18:55 -0500

    Created working python-to-text gateway

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
 #!/usr/bin/env python
 
-import pickle
-
-def sendText(message):
+def sendText(message, credentials):
     '''Send a text containing a message'''
     
-    #TODO: create network code here
+    import smtplib
     
-    #placeholder
-    print( "Simulated sending of: {}".format(message))
+    SMTP_SERVER = 'smtp.gmail.com'
+    SMTP_PORT = 587
     
-    return
+    sender = credentials['login']
+    #recipient = credentials['phone']
+    #test email
+    recipient = "litwhistle@mailinator.com"
+    password = credentials['password']
+    subject = 'SIMULATED'
+    
+    from time import gmtime, strftime
+    timeNow = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    
+    body = 'SIMULATED disaster drill at {}'.format(timeNow)
+    
+    body = "" + body + ""
+    
+    headers = ["From: " + sender, "Subject: " + subject, "To: " + recipient, "MIME-Version: 1.0", "Content-Type: text/plain"]
+    
+    headers = "\r\n".join(headers)
+    
+    session = smtplib.SMTP(SMTP_SERVER,SMTP_PORT)
+    
+    session.ehlo()
+    session.starttls()
+    session.ehlo
+    session.login(sender,password)
+    
+    session.sendmail(sender, recipient, headers + "\r\n\r\n" + body)
+    
+    session.close()
+    #session.quit()
 
 def getCredentials():
-    
+    '''Get gmail login credentials'''
+
+    import pickle
+
     with open('./PRIVATE/credentials','rb') as newCredentials:
         credentials = pickle.loads(newCredentials.read())
-    
+
     return credentials
 
 def main():
     
-    getCredentials()
+    sendText("hello",getCredentials())
     
 if __name__ == '__main__':
     main()
