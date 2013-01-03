8b7aa04f50194763d187a1da652be3d87f4ed251
Steve Vaught <Steven.D.Vaught@gmail.com>
2013-01-02 23:41:53 -0500

    Added status, implemented multiple recepients

--- main.py
+++ main.py
@@ -5,108 +5,133 @@ def getDisaster():
     
     disasters = (
         #natural
-        {'description' : 'small fire'},
-        {'description' : 'whole room on fire'},
-        {'description' : 'building on fire'},
-        {'description' : 'tornado warning'},
-        {'description' : 'flood warning'},
-        {'description' : 'volcanic eruption'},
-        {'description' : 'earthquake'},
-        {'description' : 'blizzard'},
-        {'description' : 'icy roads'},
-        {'description' : 'zero visibility'},
+        {'description' : 'Small fire'},
+        {'description' : 'Whole room on fire'},
+        {'description' : 'Building on fire'},
+        {'description' : 'Tornado warning'},
+        {'description' : 'Flood warning'},
+        {'description' : 'Volcanic eruption'},
+        {'description' : 'Earthquake'},
+        {'description' : 'Blizzard'},
+        {'description' : 'Icy roads'},
+        {'description' : 'Zero visibility'},
         
         ##silly
-        #{'description' : 'zombie apocalypse'},
-        #{'description' : 'single raptor attack'},
-        #{'description' : 'multiple raptor attack'},
+        #{'description' : 'Zombie apocalypse'},
+        #{'description' : 'Single raptor attack'},
+        #{'description' : 'Multiple raptor attack'},
         
         #social / military / crime
-        {'description' : 'riot in progress'},
-        {'description' : 'unruly crowd approaching'},
-        {'description' : 'enemy air raid'},
-        {'description' : 'drunk driver spotted'},
-        {'description' : 'two men fighting'},
-        {'description' : 'two women fighting'},
-        {'description' : 'child alone and crying'},
-        {'description' : 'man beating man'},
-        {'description' : 'man beating woman'},
-        {'description' : 'woman beating man'},
-        {'description' : 'group beating man'},
-        {'description' : 'group beating cop'},
-        {'description' : 'rape in progress nearby, one rapist'},
-        {'description' : 'rape in progress nearby, multiple rapists'},
-        {'description' : 'police officer shot'},
-        {'description' : 'police foot chase'},
-        {'description' : 'police car chase'},
-        {'description' : 'being pursued on foot by individual'},
-        {'description' : 'being pursued on foot by group'},
-        {'description' : 'being pursued in vehicle by vehicle'},
+        {'description' : 'Riot in progress'},
+        {'description' : 'Unruly crowd approaching'},
+        {'description' : 'Enemy air raid'},
+        {'description' : 'Drunk driver spotted'},
+        {'description' : 'Two men fighting'},
+        {'description' : 'Two women fighting'},
+        {'description' : 'Child alone and crying'},
+        {'description' : 'Man beating man'},
+        {'description' : 'Man beating woman'},
+        {'description' : 'Woman beating man'},
+        {'description' : 'Group beating man'},
+        {'description' : 'Group beating cop'},
+        {'description' : 'Rape in progress nearby, one rapist'},
+        {'description' : 'Rape in progress nearby, multiple rapists'},
+        {'description' : 'Police officer shot'},
+        {'description' : 'Police foot chase'},
+        {'description' : 'Police car chase'},
+        {'description' : 'Being pursued on foot by individual'},
+        {'description' : 'Being pursued on foot by group'},
+        {'description' : 'Being pursued in vehicle by vehicle'},
         
         #terror
-        {'description' : 'shooter in the area'},
-        {'description' : 'car bomb explosion'},
-        {'description' : 'car bomb threat'},
-        {'description' : 'pipe bomb threat'},
-        {'description' : 'mail bomb threat'},
-        {'description' : 'car bomb discovered, detonation in 5 minutes'},
-        {'description' : 'pipe bomb discovered, detonation in 5 minutes'},
-        {'description' : 'mail bomb discovered, detonation in 5 minutes'},
-        {'description' : 'incoming sniper fire from long range'},
+        {'description' : 'Shooter in the area'},
+        {'description' : 'Car bomb explosion'},
+        {'description' : 'Car bomb threat'},
+        {'description' : 'Pipe bomb threat'},
+        {'description' : 'Mail bomb threat'},
+        {'description' : 'Car bomb discovered, detonation in 5 minutes'},
+        {'description' : 'Pipe bomb discovered, detonation in 5 minutes'},
+        {'description' : 'Mail bomb discovered, detonation in 5 minutes'},
+        {'description' : 'Incoming sniper fire from long range'},
         
         #mystery / misc
-        {'description' : 'distant explosion'},
-        {'description' : 'toxic gas release'},
-        {'description' : 'nerve gas release'},
-        {'description' : 'smell of natural gas'},
-        {'description' : 'unidentified loud noise'},
-        {'description' : 'radiation leak'},
-        {'description' : 'woman screaming in distance'},
-        {'description' : 'loud shouts in distance'},
-        {'description' : 'loud bang, close by'},
+        {'description' : 'Distant explosion'},
+        {'description' : 'Toxic gas release'},
+        {'description' : 'Nerve gas release'},
+        {'description' : 'Strong smell of natural gas'},
+        {'description' : 'Unidentified loud noise'},
+        {'description' : 'Radiation leak'},
+        {'description' : 'Woman screaming in distance'},
+        {'description' : 'Loud shouts in distance'},
+        {'description' : 'Loud bang, close by'},
         
         #accidents
-        {'description' : 'small aircraft crash nearby'},
-        {'description' : 'airliner crash nearby'},
-        {'description' : 'train derailed nearby'},
-        {'description' : 'car-train collision nearby'},
-        {'description' : 'head-on car accident nearby'},
+        {'description' : 'Small aircraft crash nearby'},
+        {'description' : 'Airliner crash nearby'},
+        {'description' : 'Train derailed nearby'},
+        {'description' : 'Car-train collision nearby'},
+        {'description' : 'Head-on car accident nearby'},
         {'description' : 'T-bone car accident nearby'},
         
         #medical
-        {'description' : 'nosebleed'},
-        {'description' : 'man bleeding without obvious cause'},
-        {'description' : 'nauseating smell'},
-        {'description' : 'sudden vomiting'},
-        {'description' : 'person begins choking nearby'},
-        {'description' : 'person unresponsive after falling, hitting head'}
+        {'description' : 'Nosebleed'},
+        {'description' : 'Man bleeding without obvious cause'},
+        {'description' : 'Nauseating smell'},
+        {'description' : 'Sudden vomiting'},
+        {'description' : 'Person begins choking nearby'},
+        {'description' : 'Person unresponsive after falling, hitting head'}
         
         )
     
     return random.choice(disasters)['description']
 
+def getStatus():
+    '''Returns various status effects'''
+    
+    import random
+    
+    # Chances (out of 100)
+    armedChance = 50
+    hurtChance = 20
+    aloneChance = 60
+    groupChance = 40
+    
+    isArmed = "Armed, " if (random.randint(1,100) < armedChance) else "Unarmed, "
+    
+    isHurt = "injured, " if (random.randint(1,100) < hurtChance) else ""
+    
+    #must determine this first, might or might not be used
+    isGroup = "with group." if (random.randint(1,100) < groupChance) else "with friend."
+    
+    isAlone = "alone." if (random.randint(1,100) < aloneChance) else isGroup
+    
+    body = isArmed + isHurt + isAlone
+    
+    #print(body)
+    
+    return(body)
+
 def main():
     
-    from sendText import sendText, getCredentials
+    from sendText import sendText, getCredentials, getRecepients
     
-    subject = 'SIMULATED Crisis'
-    #recipient = "litwhistle@mailinator.com"
+    subject = 'SIMULATED Alert'
     
-    recipient = getCredentials()['phone']
+    #recepients = getRecepients()
     
-    from time import gmtime, strftime
-    timeNow = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    #recepients = ('litwhistle@mailinator.com')
     
-    body = 'SIMULATED disaster drill at {}'.format(timeNow)
+    body = 'SIMULATED alert. Situation: {}. Your status: {}'.format( getDisaster(), getStatus() )
     
-    #print (recipient)
+    print (body)
     
-    sendText(subject, body, getCredentials(), recipient)
+    for eachRecepient in recepients:
+        sendText(subject, body, getCredentials(), eachRecepient)
 
 def runDebug():
-    print( getDisaster() )
+    print( getRecepients() )
 
 
 if __name__ == '__main__':
-    #main()
-    runDebug()
+    main()
+    #runDebug()
